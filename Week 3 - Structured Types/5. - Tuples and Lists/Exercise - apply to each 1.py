ecd8f3d383ba1f6c098508e7ed4f8902923d4b4e
jillvp <52222203+jillvp@users.noreply.github.com>
2019-08-07 12:27:50 +0200

    Update Exercise - apply to each 1.py

--- Week 3 - Structured Types/5. - Tuples and Lists/Exercise - apply to each 1.py
+++ Week 3 - Structured Types/5. - Tuples and Lists/Exercise - apply to each 1.py
@@ -1,4 +1,3 @@
-
 # Exercise: apply to each 1
 
 # (5/5 points)
@@ -10,12 +9,14 @@
 #     for i in range(len(L)):
 #         L[i] = f(L[i])
 
+# Assume that
+# testList = [1, -4, 8, -9]
+
 # For each of the following questions (which you may assume is evaluated independently of the previous questions, so that testList
 # has the value indicated above), provide an expression using applyToEach, so that after evaluation testList has the indicated
 # value. You may need to write a simple procedure in each question to help with this process.
 
-# Assume that
-# testList = [1, -4, 8, -9]
+
 #  >>> print(testList)
 #  [1, 4, 8, 9]
 
