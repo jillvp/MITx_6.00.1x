fda4c979d93b844088a26fd423d237aa0e1f0b58
jillvp <52222203+jillvp@users.noreply.github.com>
2019-08-07 11:40:27 +0200

    Exercise - while exercise 1

---
+++ Week 1 - Python Basics/2. - Core Elements of Programs/Exercise - while exercise 1.py
@@ -0,0 +1,21 @@
+# Exercise: while exercise 1
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 5 minutes
+
+# In this problem you'll be given a chance to practice writing some while loops.
+
+# 1. Convert the following into code that uses a while loop.
+
+# print 2
+# prints 4
+# prints 6
+# prints 8
+# prints 10
+# prints Goodbye!
+
+num = 0
+while num < 10:
+    num += 2
+    print(num)
+print('Goodbye!')
