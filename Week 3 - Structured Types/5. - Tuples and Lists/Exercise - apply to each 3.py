b02e6f2b3177057e522f9f68918fdb16b9dde636
jillvp <52222203+jillvp@users.noreply.github.com>
2019-08-07 12:28:01 +0200

    Exercise - apply to each 3

---
+++ Week 3 - Structured Types/5. - Tuples and Lists/Exercise - apply to each 3.py
@@ -0,0 +1,18 @@
+# Exercise: apply to each 3
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 4 minutes
+
+# Assume that
+# testList = [1, -4, 8, -9]
+
+#  >>> print(testList)
+#  [1, 16, 64, 81]
+
+# Your Code Here
+def squared(c):
+  return c*c
+ 
+applyToEach(testList, squared)
+
+# Correct
