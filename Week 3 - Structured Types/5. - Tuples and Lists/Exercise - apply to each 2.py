981950b9b05a025ce3792a67d36e6c11cc990be0
jillvp <52222203+jillvp@users.noreply.github.com>
2019-08-07 12:27:03 +0200

    Exercise - apply to each 2

---
+++ Week 3 - Structured Types/5. - Tuples and Lists/Exercise - apply to each 2.py
@@ -0,0 +1,17 @@
+# Exercise: apply to each 2
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 4 minutes
+
+# Assume that
+# testList = [1, -4, 8, -9]
+
+#  >>> print(testList)
+#  [2, -3, 9, -8]
+
+# Your Code Here
+def addOne(b):
+        return b + 1
+applyToEach(testList, addOne)
+
+# Correct
